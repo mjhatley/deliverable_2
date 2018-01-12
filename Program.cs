544a5ba32cfa114e7e5a6ece6746126640fd923e
mjhatley <mjhatley21@gmail.com>
2018-01-11 21:52:42 -0500

    added comments

--- Program.cs
+++ Program.cs
@@ -6,20 +6,24 @@ namespace deliverables_2
     {
         public static void Main(string[] args)
         {
+            // asked for first and second date
             Console.WriteLine("ask for first date year-month-day");
             string firstDateString = Console.ReadLine();
 
             Console.WriteLine("ask for second date year-month-day");
             string secondDateString = Console.ReadLine();
 
+           //convert string to date time
             DateTime firstDate = CreateDateTime(firstDateString);
             DateTime secondDate = CreateDateTime(secondDateString);
 
+            //check to see if date time had minimum value (signals failure)
             if (firstDate == DateTime.MinValue || secondDate == DateTime.MinValue)
             {
                 return;
             }
 
+            //Takes the smaller date and subracts from larger one
             TimeSpan difference;
             if (firstDate < secondDate) {
                 difference = secondDate - firstDate;
@@ -28,15 +32,18 @@ namespace deliverables_2
                 difference = firstDate - secondDate;
             }
 
+            //takes the amount of days and converts them into hours and minutes
             int hours = difference.Days * 24;
             int minutes = hours * 60;
 
             Console.WriteLine("Days: " + difference.Days + " Hours: " + hours + " Minutes: " + minutes); 
 
         }
-
+        //makes sure year month and day are correct inputs
         private static DateTime CreateDateTime(string dateString) {
             String[] substrings = dateString.Split('-');
+
+            //checking to make sure there are 3 strings
             if (substrings.Length != 3)
             {
                 Console.WriteLine("Date Non Existent");
@@ -47,12 +54,14 @@ namespace deliverables_2
             string monthString = substrings[1];
             string dayString = substrings[2];
 
+            //making sure year string has 4 characters
             if (yearString.Length != 4)
             {
                 Console.WriteLine("Year not of Length 4");
                 return DateTime.MinValue;
             }
 
+            //converts the strings to numbers
             int year = 0;
             int month = 0;
             int day = 0;
@@ -70,12 +79,14 @@ namespace deliverables_2
                 return DateTime.MinValue;
             }
 
+            //gives minimum value if input is not 1-12 (signals error)
             if (month < 1 || month > 12)
             {
                 Console.WriteLine("Invalid Month");
                 return DateTime.MinValue;
             }
 
+            //gives minimum value if input is not 1-31 (signals error)
             if (day < 1 || day > 31)
             {
 
